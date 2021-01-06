a43b2dbda3ff2684f592261ed628e5f8a6fa2109
Rakshitha Evangeline <rakshithaevangeline29@gmail.com>
2021-01-06 10:08:52 +0530

    Add a working button to the page.

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 let Phrase = require("rakshithaevangeline-palindrome");
 
-let content = prompt("Please type a phrase here:");
-let phrase = new Phrase(content);
 
-if (phrase.palindrome()) {
-  alert(`${phrase.content} is a palindrome!`);
-} else {
-  alert(`${phrase.content} is not a palindrome.`);
-};
+function palindromeTester() {
+  let content = prompt("Please type a phrase here:");
+  let phrase = new Phrase(content);
+  
+  if (phrase.palindrome()) {
+    alert(`${phrase.content} is a palindrome!`);
+  } else {
+    alert(`${phrase.content} is not a palindrome.`);
+  };
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("click", () => {
+  palindromeTester();
+});
+});
